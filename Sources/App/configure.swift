3e1e774829d6d8c18ba8c9b53770b6204d7de842
kirkH <kirk@passivelogic.com>
2023-01-21 18:09:32 -0700

    Styling with bootstrap

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,7 +6,7 @@ import Leaf
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory)) // for serving static files?
     app.logger.logLevel = .debug
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
