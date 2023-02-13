74db3dcf61be9f5764be3f753b462004a3a1c4a9
kirkH <kirk@passivelogic.com>
2023-02-13 16:23:16 -0700

    wip: tokens

--- Sources/App/Models/Token.swift
+++ Sources/App/Models/Token.swift
@@ -1,8 +1,25 @@
-//
-//  File.swift
-//  
-//
-//  Created by Kirk Hietpas on 2/13/23.
-//
+import Vapor
+import Fluent
 
-import Foundation
+final class Token: Model, Content {
+    static let schema = "tokens"
+    
+    @ID
+    var id: UUID?
+    
+    @Field (key: "value")
+    var value: String
+    
+    @Parent(key: "userID")
+    var user: User
+    
+    
+    init() {}
+    
+    init(id: UUID? = nil, value: String, userID: User.IDValue) {
+        self.id = id
+        self.value = value
+        self.$user.id = userID
+    }
+    
+}
